7e0acae482198fd357cbb6d6fdde9a0d8786f33b
Mosi <mosi88866686@gmail.com>
2025-05-06 22:26:37 +0400

    add product edit

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { Hono } from "hono";
 import { ProductCreate } from "./endpoints/productCreate";
 import { ProductDelete } from "./endpoints/productDelete";
 import { ProductFetch } from "./endpoints/productFetch";
+import { ProductEdit } from "./endpoints/productEdit";
 import { TaskList } from "./endpoints/productList";
 
 // Start a Hono app
@@ -18,6 +19,7 @@ openapi.get("/api/products", TaskList);
 openapi.post("/api/products", ProductCreate);
 openapi.get("/api/products/:id", ProductFetch);
 openapi.delete("/api/products/:id", ProductDelete);
+openapi.put("/api/products/:id", ProductEdit);
 
 // You may also register routes for non OpenAPI directly on Hono
 // app.get('/test', (c) => c.text('Hono!'))
